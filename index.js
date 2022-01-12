af82f3baf54b8fe5e03b36f772649a5f045b37d6
Angel Rosario <arosario.us@gmail.com>
2022-01-11 19:48:51 -0500

    commit tree f4f6b924e2f7991de8fb2d3ee7d796521a102e2b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+// defines saturdayFun function declaration as specified
+function saturdayFun() {
+    const activity = 'roller-skate'
+     console.log(`This Saturday, I want to ${activity}!`);
+     const batheMyDog = 'bathe my dog!'
+     return `This Saturday, I want to ${batheMyDog}`
+}
+
+// defines mondayWork function expression as specified
+function mondayWork() {
+    const workFromHome = 'work from home.';
+    return `This Monday, I will ${workFromHome}`;
+}
+
+// defines wrapAdjective function according to the specification
+function wrapAdjective(special) {
+    const defaultSpecial = 'awesome'
+    return `You are ${defaultSpecial}`
+}
